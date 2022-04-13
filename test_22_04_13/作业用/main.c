b08e040663c9889f86d2f347f7b904595b6e60bf
SintSapphire <1473320478@qq.com>
2022-04-13 21:11:33 +0800

    练习

---
+++ test_22_04_13/作业用/main.c
@@ -0,0 +1,92 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	int a, b, c, max, min;
+//	scanf("%d %d %d", &a, &b, &c);
+//	if (a > b)
+//	{
+//		max = a;
+//		min = b;
+//	}
+//	else
+//	{
+//		max = b;
+//		min = a;
+//	}
+//	if (c > max)
+//	{
+//		max = c;
+//	}
+//	if (c < min)
+//	{
+//		min = c;
+//	}
+//	printf("%d %d %d", max, a + b + c - max - min, min);
+//
+//
+//	return 0;
+//
+//}
+
+//int main()
+//{
+//	int i = 3;
+//	while (i > 0 && i <= 100)
+//	{
+//		printf("%d\n", i);
+//		i += 3;
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//    int m = 0;
+//    int n = 0;
+//    int tmp = 0;
+//    scanf("%d %d", &m, &n);
+//
+//    while (tmp = m % n)
+//    {
+//        m = n;
+//        n = tmp;
+//    }
+//    printf("%d\n", n);
+//    return 0;
+//}
+
+//int main()
+//{
+//	int year;
+//	for (year = 1000; year >= 1000 && year <= 2000; year++)
+//		if (year % 4 == 0 && year % 100 != 0)
+//			printf("%d\n", year);
+//
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int n;
+//	for (n = 100; n <= 200; n++)
+//	{
+//		int i = 2;
+//		while(i < n)
+//		{
+//			if (n%i==0)
+//			{
+//				break;
+//			}
+//			i++;
+//		}
+//		if (i == n)
+//			printf("%d\n", n);
+//
+//	}
+//	
+//}
