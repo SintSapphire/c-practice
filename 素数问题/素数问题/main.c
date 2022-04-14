7152f1ac230df8b310385b7e5c0758bef5f0fa37
SintSapphire <1473320478@qq.com>
2022-04-14 21:14:54 +0800

    练习

---
+++ 素数问题/素数问题/main.c
@@ -0,0 +1,85 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	int n;
+//	for (n = 100; n <= 200; n++)
+//	{
+//		int i = 2;
+//		while(i < n)
+//		{
+//			if (n%i==0)
+//			{
+//				break;
+//			}
+//			i++;
+//		}
+//		if (i == n)
+//			printf("%d\n", n);
+//	}
+//}
+
+//int main()
+//{
+//	int n;
+//	for (n = 101; n <= 200; n+=2)
+//	{
+//		int i = 3;
+//		while(i < n)
+//		{
+//			if (n%i==0)
+//			{
+//				break;
+//			}
+//			i+=2;
+//		}
+//		if (i == n)
+//			printf("%d\n", n);
+//	}
+//}
+
+//int main()
+//{
+//	int n = 1;
+//	for (int i = 101; i < 200; i++)
+//	{
+//		for (int j = 2; j < i; j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				n = 0;
+//				break;
+//			}
+//		}
+//		if (n == 1)
+//			printf("%d ", i);
+//		else
+//			n = 1;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//
+//	int m = 0;
+//	for (int i = 2; i < n; i++) {
+//		if (n % i == 0)
+//		{
+//			m = 1;
+//			break;
+//		}
+//	}
+//	if (m == 0) {
+//		printf("%d是素数\n", n);
+//	}
+//	else
+//	{
+//		printf("%d不是素数\n", n);
+//	}
+//
+//	return 0;
+//}
